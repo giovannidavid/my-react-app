ea428a39a71072a688f290bfe79523624f2a3302
giovannidavid <giovannidavid67@gmail.com>
2024-10-23 17:00:17 +0200

    pokemoncard

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 const PokemonCard = () => {
 
+
+   
 return (
 <figure>
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
@@ -9,6 +11,8 @@ return (
 </figure>
 );
 }
+
+
 
 
 export default PokemonCard ;
